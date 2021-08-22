179ce292120fbb7cd8646d5c01072abb7a716f5e
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2021-08-21 21:00:42 -0700

    Deploy to shinyapps.io

--- app.R
+++ app.R
@@ -2,31 +2,42 @@ library(shiny)
 library(ggplot2)
 library(plotly)
 
-ui <- fluidPage(
-    titlePanel("Sample Size Estimator"),
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("baseline",
-                        "Baseline:",
-                        min = 0,
-                        max = 1,
-                        value = 0.5),
-            sliderInput("alpha",
-                        "Alpha:",
-                        min = 0, 
-                        max = 1,
-                        value = 0.05),
-            sliderInput("power",
-                        "Power:",
-                        min = 0, 
-                        max = 1,
-                        value = 0.8)
+ui <- navbarPage("Sample Size Estimator",
+        tabPanel("Proportions",
+            sidebarLayout(
+                sidebarPanel(
+                    sliderInput("baseline",
+                                "Baseline",
+                                min = 0,
+                                max = 1,
+                                value = 0.5),
+                    sliderInput("alpha",
+                                "Alpha",
+                                min = 0, 
+                                max = 1,
+                                value = 0.05),
+                    sliderInput("power",
+                                "Power",
+                                min = 0, 
+                                max = 1,
+                                value = 0.8)
+                ),
+                mainPanel(
+                   plotlyOutput("plot")
+                )
+            )
         ),
-        mainPanel(
-           plotlyOutput("plot")
+        tabPanel("About",
+                 p("This is an app for estimating the sample size needed to run an experiment. 
+                   It takes 3 parameters as input: baseline, alpha, and power."),
+                 h3("Baseline"),
+                 p("The baseline value of the variable being tested. The variable's proportion or average in the control group."),
+                 h3("Alpha"),
+                 p("Also known as the significance level, alpha is the experiment's Type I error (false positive) rate."),
+                 h3("Power"),
+                 p("1 - Beta, the Type II error or false negative rate. Power represents the probability the experiment will detect a given effect size at a given significance level.")
         )
     )
-)
 
 server <- function(input, output) {
     output$plot <- renderPlotly({
